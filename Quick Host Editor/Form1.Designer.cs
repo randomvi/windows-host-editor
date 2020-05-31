77498d3ca2b4411e405b18a4c29611009911726c
Randomvi <randomvii@outlook.com>
2020-05-31 12:30:09 +0200

    initial project

---
+++ Quick Host Editor/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace Quick_Host_Editor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.iptext = new System.Windows.Forms.TextBox();
+            this.hosttext = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.ipinfo = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(84, 302);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(128, 43);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "SAVE ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // iptext
+            // 
+            this.iptext.Location = new System.Drawing.Point(196, 29);
+            this.iptext.Name = "iptext";
+            this.iptext.Size = new System.Drawing.Size(163, 20);
+            this.iptext.TabIndex = 1;
+            // 
+            // hosttext
+            // 
+            this.hosttext.Location = new System.Drawing.Point(196, 64);
+            this.hosttext.Name = "hosttext";
+            this.hosttext.Size = new System.Drawing.Size(163, 20);
+            this.hosttext.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(128, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "IP Address";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(128, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Host Name";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(264, 302);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(106, 42);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "CLEAR";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // ipinfo
+            // 
+            this.ipinfo.BackColor = System.Drawing.Color.LightGray;
+            this.ipinfo.Location = new System.Drawing.Point(449, 46);
+            this.ipinfo.Multiline = true;
+            this.ipinfo.Name = "ipinfo";
+            this.ipinfo.ReadOnly = true;
+            this.ipinfo.Size = new System.Drawing.Size(237, 205);
+            this.ipinfo.TabIndex = 6;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.LightGreen;
+            this.button3.Location = new System.Drawing.Point(517, 302);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(105, 43);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "HELP";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(389, 304);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(107, 39);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Net IP";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gainsboro;
+            this.ClientSize = new System.Drawing.Size(733, 385);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.ipinfo);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.hosttext);
+            this.Controls.Add(this.iptext);
+            this.Controls.Add(this.button1);
+            this.HelpButton = true;
+            this.Name = "Form1";
+            this.Text = "Host Editor";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox iptext;
+        private System.Windows.Forms.TextBox hosttext;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox ipinfo;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Quick Host Editor/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Net;
+using System.Windows.Forms;
+
+namespace Quick_Host_Editor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+            ipinfo.Text = "LOCALHOST IP: 127.0.0.1 \n";
+        }
+    
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string address="C:\\Windows\\System32\\Drivers\\etc\\hosts";
+            FileStream fik = new FileStream(address, FileMode.Append, FileAccess.Write);
+            StreamWriter hfile = new StreamWriter(fik);
+            hfile.WriteLine("\n{0} {1}",iptext.Text,hosttext.Text);
+            MessageBox.Show("Saved successfully");
+            hfile.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+           iptext.Clear();
+           hosttext.Clear();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Please make sure your hosts file \n is allowed to be edited by an application");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            IPHostEntry hoster;
+            string localIpAddress = "";
+            hoster = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (IPAddress ip in hoster.AddressList)
+            {
+                if (ip.AddressFamily.ToString() == "InterNetwork")
+                {
+                    localIpAddress = ip.ToString();
+                    ipinfo.Text = "IP Address :  " + localIpAddress;
+                }
+            }
+            if (localIpAddress == "127.0.0.1")
+            {
+                MessageBox.Show("Please Check your WiFi");
+            }
+        }
+
+        
+
+        
+
+    }
+}
